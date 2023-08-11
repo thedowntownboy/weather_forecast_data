921ce3ba429358b386d25adde51cf2bc18340bb6
Owner <joywin16@gmail.com>
2023-08-11 14:27:29 +0100

    day 32 added graph (plotly) and given fake data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 # st.set_page_config(layout="wide")
 
@@ -8,3 +9,17 @@ days = st.slider("Forecast Days", min_value=1, max_value=5)
 option = st.selectbox("Select data to view", ('Temperature', 'Sky'))
 
 st.subheader(f"{option} for the next {days} days in {place}")
+
+
+# fake data
+def get_data(days):
+    date = ['11-08-2023', '12-08-2023', '13-08-2023']
+    temperature = [19, 17, 25]
+    temperature = [days * i for i in temperature]
+    return date, temperature
+
+
+d, t = get_data(days)
+# plotly is used to render the graph as it works with streamlit
+figure = px.line(x=d, y=t, labels={"x": "date", "y": "temperature in c"})
+st.plotly_chart(figure)
