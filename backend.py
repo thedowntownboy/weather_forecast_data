6f8438de2672ad371259cd5430cc157a5bece530
Owner <joywin16@gmail.com>
2023-08-14 16:21:50 +0100

    day 32 data called to main page , real data graph and sky data with img is added

---
+++ backend.py
@@ -0,0 +1,16 @@
+import requests
+
+api_key = "a19de81303b89f94f914f6431dbd76c8"
+
+
+def get_data(place="tokyo", days=None, option=None):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
+    response = requests.get(url)
+    content = response.json()
+    filtered_content = content['list'][:8*days]
+
+    return filtered_content
+
+
+if __name__ == "__main__":
+    print(get_data(place="london", days=2, option="Temperature"))
--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 # st.set_page_config(layout="wide")
-
+# widgets are added in the home page
 st.title("weather forecast for the next days")
 place = st.text_input("Place :")
 days = st.slider("Forecast Days", min_value=1, max_value=5)
 option = st.selectbox("Select data to view", ('Temperature', 'Sky'))
-
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    # filtered data is called
+    filtered_content = get_data(place, days)
 
-# fake data
-def get_data(days):
-    date = ['11-08-2023', '12-08-2023', '13-08-2023']
-    temperature = [19, 17, 25]
-    temperature = [days * i for i in temperature]
-    return date, temperature
+    # plotly is used to render the graph as it works with streamlit
+    if option == "Temperature":
+        temperature = [item['main']['temp'] for item in filtered_content]
+        dates = [item['dt_txt'] for item in filtered_content]
+        figure = px.line(x=dates, y=temperature,
+                         labels={"x": "date", "y": "temperature in c"})
+        st.plotly_chart(figure)
 
-
-d, t = get_data(days)
-# plotly is used to render the graph as it works with streamlit
-figure = px.line(x=d, y=t, labels={"x": "date", "y": "temperature in c"})
-st.plotly_chart(figure)
+    if option == "Sky":
+        filtered_content = [item['weather'][0]['main'] for item in filtered_content]
+        images = {"Clear": 'images/clear.png', "Clouds": 'images/cloud.png',
+                  "Rain": 'images/rain.png', "Snow": 'images/snow.png'}
+        image_path = [images[condition] for condition in filtered_content]
+        st.image(image_path, width=115)
